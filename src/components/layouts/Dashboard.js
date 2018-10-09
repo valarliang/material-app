c1d340270ba44d055190d907fa9d58d2b84faef3
梁琰沁 <1780304097@qq.com>
2018-10-09 17:39:16 +0800

    init

---
+++ src/components/layouts/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+  render(){
+    return (
+      <h1>hello</h1>
+    );
+  }
+}
+
+export default Dashboard;
